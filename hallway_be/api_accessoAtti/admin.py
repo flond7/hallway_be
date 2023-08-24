a756bbceb3f1b8f7fd1e2d514b2967ffd1fb7595
flond7 <elisa.utilivenza@gmail.com>
2023-08-24 08:37:37 +0200

    added accessoAtti to admin panel

--- hallway_be/api_accessoAtti/admin.py
+++ hallway_be/api_accessoAtti/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import accessoAtti
+
+from django.contrib import admin
+
+# Register your models here.
+# it allows to have a user friendly interface to input data in the db
+admin.site.register(accessoAtti)
