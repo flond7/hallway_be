bec47d3268208b441aaeceec8ac196226d97fb9a
flond7 <elisa.utilivenza@gmail.com>
2023-08-25 13:12:48 +0200

    simplified model

--- hallway_be/api_accessoAtti/models.py
+++ hallway_be/api_accessoAtti/models.py
@@ -4,16 +4,16 @@ from django.db import models
 
 class accessoAtti(models.Model):
  
-  requestProtocol = models.CharField(max_length = 10, blank=False)
-  requestDate = models.CharField(max_length = 20, blank=False)
-  requestApplicant = models.CharField(max_length = 80, blank=False)
-  accesstype = models.CharField(max_length = 80, blank=False)
-  topic = models.TextField(default="", blank=False)
-  others = models.CharField(max_length = 20, blank=False)
-  responsable = models.CharField(max_length = 40, blank=False)
-  answerResult = models.CharField(max_length = 20, blank=False)
-  answerProtocol = models.CharField(max_length = 10, blank=False)
-  answerDate = models.CharField(max_length = 20, blank=False)
+  requestProtocol = models.CharField(max_length = 10, blank=True)
+  requestDate = models.CharField(max_length = 20, blank=True)
+  requestApplicant = models.CharField(max_length = 80, blank=True)
+  accesstype = models.CharField(max_length = 80, blank=True)
+  topic = models.TextField(default="", blank=True)
+  others = models.CharField(max_length = 20, blank=True)
+  responsable = models.CharField(max_length = 40, blank=True)
+  answerResult = models.CharField(max_length = 20, blank=True)
+  answerProtocol = models.CharField(max_length = 10, blank=True)
+  answerDate = models.CharField(max_length = 20, blank=True)
   answerNote = models.TextField(default="", blank=True)
 
   class Meta:
