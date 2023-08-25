4e4ed3a9dea9be69536d219ed7504c76eaf869f2
flond7 <elisa.utilivenza@gmail.com>
2023-08-25 13:12:57 +0200

    coherence in urls

--- hallway_be/api_accessoAtti/urls.py
+++ hallway_be/api_accessoAtti/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
   #path('login', views.index, name="login"),
-  path('access_new', views.access_create, name="new_access"), 
+  path('access_new', views.access_create, name="access_new"), 
   #path('access_delete', views.access_create, name="access_delete"), 
   path('access_list', views.access_list_all, name="access_list"), 
   #path('access_record', views.access_create, name="access_record"), 
