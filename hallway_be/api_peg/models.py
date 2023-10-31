6e8797039bc19714e74cebd8c45f7450ef0a9ea4
flond7 <elisa.utilivenza@gmail.com>
2023-10-31 08:56:34 +0100

    obbiettivo model

--- hallway_be/api_peg/models.py
+++ hallway_be/api_peg/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from api_user.models import PAUser
 
 # Create your models here.
+
+class obbiettivo(models.Model):
+ 
+  year = models.CharField(max_length = 10, blank=True)
+  office = models.CharField(max_length = 10, blank=True)
+  name = models.CharField(max_length = 10, blank=True)
+  descripition = models.CharField(max_length = 600, blank=True)
+  weight = models.IntegerField()
+  responsable = models.CharField(max_length = 40, blank=True)
+  completation3006 = models.IntegerField()
+  completation3112 = models.IntegerField()
+  people = models.ManyToManyField(PAUser)
+
+  class Meta:
+    ordering = []
+
+  def __str__(self):
+    return self
