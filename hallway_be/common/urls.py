15294be002b3c1b36f9eac0077f6926850829afa
flond7 <elisa.utilivenza@gmail.com>
2023-11-02 10:54:43 +0100

    get constants_list

--- hallway_be/common/urls.py
+++ hallway_be/common/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
   path('get_csrf_token', views.get_csrf_token, name="get_csrf_token"),
   path('user_log', views.user_log, name="user_log"),
   path('get_user_profiles_auth/<int:pk>', views.get_user_profiles_auth, name="get_user_profiles_auth"),
-  #path('user_log', views.user_log, name="user_log"),
+  path('constants_list', views.constants_list, name="constants_list"),
  ] 
--- hallway_be/common/views.py
+++ hallway_be/common/views.py
@@ -82,3 +82,10 @@ def get_user_profiles_auth(request, pk):
         except UserProfile.DoesNotExist:
             return JsonResponse({"error": "Profilo utente non trovato"}, status=404)
 
+
+def constants_list(request):
+  if request.method == "GET":
+    constants = {
+        'main_office_choices': MAIN_OFFICE_CHOICES,
+    }
+  return JsonResponse(constants, status=201)
