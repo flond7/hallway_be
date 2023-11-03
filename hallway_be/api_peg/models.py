ccfbc7a862cfa59702d840022f852af45ccde230
flond7 <elisa.utilivenza@gmail.com>
2023-11-03 09:31:26 +0100

    fixed problem with foreignkey and manytomany field

--- hallway_be/api_peg/models.py
+++ hallway_be/api_peg/models.py
@@ -10,12 +10,19 @@ class goalPeg(models.Model):
   name = models.CharField(max_length = 10, blank=True)
   descripition = models.CharField(max_length = 600, blank=True)
   weight = models.IntegerField()
-  manager = models.CharField(max_length = 40, blank=True)
+  #manager = models.CharField(max_length = 40, blank=True)
+  manager = models.ForeignKey(
+    PAUser,
+    related_name="goal_manager",  # name that identifies the relationship. Django creates that in aut mode with the model name and _set so if you have multiple foreignKeys or ManyToMany it's better to custom name them to avoid conflicts
+    on_delete=models.PROTECT,      # prevents from deleting the PAUser linked here
+  )
   percent_3006 = models.IntegerField()
   weight_3006 = models.IntegerField()
   percent_3112 = models.IntegerField()
   weight_3112 = models.IntegerField()
-  involvedPeople = models.ManyToManyField(PAUser)
+  involvedPeople = models.ManyToManyField(
+    PAUser, 
+    related_name="goal_involvedPeople")
 
   class Meta:
     ordering = []
