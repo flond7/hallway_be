0827abe25a306e1da70ba99ad2565b4c6bf9f871
flond7 <elisa.utilivenza@gmail.com>
2023-11-07 09:30:49 +0100

    changed office to foreignkey

--- hallway_be/api_peg/serializers.py
+++ hallway_be/api_peg/serializers.py
@@ -1,9 +1,12 @@
 from .models import goalPeg
+from api_user.serializers import PAOfficeSerializer
 from rest_framework import serializers
 
 # UserListSerializer 
 # retrieves only the data needed to work with api_peg and create a list of users
 class goalPegSerializer(serializers.ModelSerializer):
+    offices = PAOfficeSerializer(many=True, read_only=True)
+
     class Meta:
         model = goalPeg
         fields = '__all__'
