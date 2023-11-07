4bf6bf014a982485a68025a7a173a83fad996139
flond7 <elisa.utilivenza@gmail.com>
2023-11-07 14:44:09 +0100

    fixed sending back office id and name

--- hallway_be/api_peg/serializers.py
+++ hallway_be/api_peg/serializers.py
@@ -5,8 +5,8 @@ from rest_framework import serializers
 # UserListSerializer 
 # retrieves only the data needed to work with api_peg and create a list of users
 class goalPegSerializer(serializers.ModelSerializer):
-    offices = PAOfficeSerializer(many=True, read_only=True)
+    office = PAOfficeSerializer(many=False, read_only=True)
 
     class Meta:
         model = goalPeg
-        fields = '__all__'
+        fields = ('id', 'year', 'office','name','description','weight', 'manager', 'percent_3006', 'weight_3006', 'percent_3112', 'weight_3112', 'type','involvedPeople')
