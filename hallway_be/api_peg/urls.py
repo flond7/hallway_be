552539ecc9ee180983615ddf3c8e12daa6ddbe8d
flond7 <elisa.utilivenza@gmail.com>
2023-11-10 07:49:37 +0100

    updated url

--- hallway_be/api_peg/urls.py
+++ hallway_be/api_peg/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
   path('goal_new', views.goal_create, name="goal_new"), 
-  path('goals_new', views.create_multiple_goals, name='goals_new'),
+  path('create_multiple_goals', views.create_multiple_goals, name='create_multiple_goals'),
   path('delete_multiple_goals', views.delete_multiple_goals, name='delete_multiple_goals'),
   path('get_person_results', views.get_person_results, name='get_person_results'),
   path('get_po_results', views.get_po_results, name='get_po_results'),
