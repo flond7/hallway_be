77561347d65b7e8ae5c924e393662b15d8867d05
flond7 <elisa.utilivenza@gmail.com>
2024-01-08 18:02:16 +0100

    added strings of office names to offices arrays

--- hallway_be/api_user/serializers.py
+++ hallway_be/api_user/serializers.py
@@ -1,5 +1,6 @@
 from .models import customUser, askUser, PAUser, PAOffice, PACredential
 from rest_framework import serializers
+from .modelsConstants import LAN_OFFICE_CHOICES, LAN_ROLES_CHOICES, MAIL_OFFICE_CHOICES, ADWEB_OFFICE_CHOICES, ADWEB_ROLES_CHOICES, ASCOT_OFFICE_CHOICES, ASCOT_ROLES_CHOICES, GIFRA_OFFICE_CHOICES, GIFRA_ROLES_CHOICES
 
 # UserListSerializer 
 # retrieves only the data needed to work with api_peg and create a list of users
@@ -34,6 +35,40 @@ class PAOfficeAndPOSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'manager')
 
 class PACredentialSerializer(serializers.ModelSerializer):
+    user = PAUserPEGSerializer()
+
+
+    def to_representation(self, instance):
+        # this is required to get the data and then modify them as you want
+        ret = super().to_representation(instance)
+
+        # create a dictionary 
+        lan_office_mapping = {code: label for code, label in LAN_OFFICE_CHOICES}
+        mail_office_mapping = {code: label for code, label in MAIL_OFFICE_CHOICES}
+        adweb_office_mapping = {code: label for code, label in ADWEB_OFFICE_CHOICES}
+        ascot_office_mapping = {code: label for code, label in ASCOT_OFFICE_CHOICES}
+        gifra_office_mapping = {code: label for code, label in GIFRA_OFFICE_CHOICES}
+
+        #lan_role_mapping = {code: label for code, label in LAN_ROLES_CHOICES}
+        adweb_role_mapping = {code: label for code, label in ADWEB_ROLES_CHOICES}
+        ascot_role_mapping = {code: label for code, label in ASCOT_ROLES_CHOICES}
+        gifra_role_mapping = {code: label for code, label in GIFRA_ROLES_CHOICES}
+
+        # look for the label inside the dictionary based on the code and then return it
+        ret['lanOffice'] = [lan_office_mapping.get(code) for code in ret['lanOffice']]
+        ret['mailOffice'] = [mail_office_mapping.get(code) for code in ret['mailOffice']]
+        ret['adwebOffice'] = [adweb_office_mapping.get(code) for code in ret['adwebOffice']]
+        ret['ascotOffice'] = [ascot_office_mapping.get(code) for code in ret['ascotOffice']]
+        ret['gifraOffice'] = [gifra_office_mapping.get(code) for code in ret['gifraOffice']]
+
+        
+        #ret['lanRole'] = [lan_role_mapping.get(code) for code in ret['lanRole']]
+        ret['adwebRole'] = [adweb_role_mapping.get(code) for code in ret['adwebRole']]
+        ret['ascotRole'] = [ascot_role_mapping.get(code) for code in ret['ascotRole']]
+        ret['gifraRole'] = [gifra_role_mapping.get(code) for code in ret['gifraRole']]
+
+        return ret
+
     class Meta:
         model = PACredential
         fields = '__all__'
